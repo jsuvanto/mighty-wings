d2aaea99f5cedf232ba20ecf6021a23a1eb08db0
Juha Suvanto <jsuvanto@iki.fi>
2019-09-13 19:11:38 +0300

    added empty game controller script

---
+++ Assets/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public MapGenerator mapGenerator;
+
+    void Start()
+    {
+
+        // start menu
+
+        // spawn players
+
+        // assign controls to players
+
+        // disable main camera, spawn cameras for each player
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
