f4a9e62dd49f7fe73d66ce606b719fca251826e7
Juha Suvanto <jsuvanto@iki.fi>
2019-09-13 19:19:37 +0300

    player ship now collides with cave

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -4,36 +4,71 @@ using UnityEngine;
 public class MeshGenerator : MonoBehaviour
 {
 
-    public SquareGrid squareGrid;
-    List<Vector3> vertices;
-    List<int> triangles;
-
     public bool Generate;
+    public SquareGrid SquareGrid;
+    public MeshFilter Cave;
+
+    private List<Vector3> _vertices;
+    private List<int> _triangles;
+    
+    private Dictionary<int, List<Triangle>> _triangleDictionary = new Dictionary<int, List<Triangle>>();
+    private List<List<int>> _outlines = new List<List<int>>();
+    private HashSet<int> _checkedVertices = new HashSet<int>();
 
     public void GenerateMesh(CaveTile[,] caveMap, float squareSize)
     {
 
         if (!Generate) return;
 
-        squareGrid = new SquareGrid(caveMap, squareSize);
+        _triangleDictionary.Clear();
+        _outlines.Clear();
+        _checkedVertices.Clear();
 
-        vertices = new List<Vector3>();
-        triangles = new List<int>();
+        SquareGrid = new SquareGrid(caveMap, squareSize);
 
-        for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+        _vertices = new List<Vector3>();
+        _triangles = new List<int>();
+
+        for (int x = 0; x < SquareGrid.Width; x++)
         {
-            for (int y = 0; y < squareGrid.squares.GetLength(1); y++)
+            for (int y = 0; y < SquareGrid.Height; y++)
             {
-                TriangulateSquare(squareGrid.squares[x, y]);
+                TriangulateSquare(SquareGrid.Squares[x, y]);
             }
         }
 
         Mesh mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        Cave.mesh = mesh;
 
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
+        mesh.vertices = _vertices.ToArray();
+        mesh.triangles = _triangles.ToArray();
         mesh.RecalculateNormals();
+
+        Generate2DColliders();
+    }
+
+    void Generate2DColliders()
+    {
+
+        EdgeCollider2D[] currentColliders = gameObject.GetComponents<EdgeCollider2D>();
+        for (var i = 0; i < currentColliders.Length; i++)
+        {
+            Destroy(currentColliders[i]);
+        }
+
+        CalculateMeshOutlines();
+
+        foreach (var outline in _outlines)
+        {
+            EdgeCollider2D edgeCollider = gameObject.AddComponent<EdgeCollider2D>();
+            Vector2[] edgePoints = new Vector2[outline.Count];
+
+            for (var i = 0; i < outline.Count; i++)
+            {
+                edgePoints[i] = new Vector2(_vertices[outline[i]].x, _vertices[outline[i]].y);
+            }
+            edgeCollider.points = edgePoints;
+        }
     }
 
     void TriangulateSquare(Square square)
@@ -118,103 +153,232 @@ public class MeshGenerator : MonoBehaviour
         {
             if (points[i].vertexIndex == -1)
             {
-                points[i].vertexIndex = vertices.Count;
-                vertices.Add(points[i].position);
+                points[i].vertexIndex = _vertices.Count;
+                _vertices.Add(points[i].position);
             }
         }
     }
 
     void CreateTriangle(Node a, Node b, Node c)
     {
-        triangles.Add(a.vertexIndex);
-        triangles.Add(b.vertexIndex);
-        triangles.Add(c.vertexIndex);
+        _triangles.Add(a.vertexIndex);
+        _triangles.Add(b.vertexIndex);
+        _triangles.Add(c.vertexIndex);
+        Triangle triangle = new Triangle(a.vertexIndex, b.vertexIndex, c.vertexIndex);
+        AddTriangleToDictionary(triangle.vertexIndexA, triangle);
+        AddTriangleToDictionary(triangle.vertexIndexB, triangle);
+        AddTriangleToDictionary(triangle.vertexIndexC, triangle);
     }
 
-    public class SquareGrid
+    void AddTriangleToDictionary(int vertexIndexKey, Triangle triangle)
     {
-        public Square[,] squares;
-
-        public SquareGrid(CaveTile[,] caveMap, float squareSize)
+        if (_triangleDictionary.ContainsKey(vertexIndexKey))
         {
-            int nodeCountX = caveMap.GetLength(0);
-            int nodeCountY = caveMap.GetLength(1);
-            float mapWidth = nodeCountX * squareSize;
-            float mapHeight = nodeCountY * squareSize;
+            _triangleDictionary[vertexIndexKey].Add(triangle);
+        }
+        else
+        {
+            _triangleDictionary.Add(vertexIndexKey, new List<Triangle> { triangle });
+        }
+    }
 
-            ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
-
-            for (int x = 0; x < nodeCountX; x++)
+    void CalculateMeshOutlines()
+    {
+        for (int vertexIndex = 0; vertexIndex < _vertices.Count; vertexIndex++)
+        {
+            if (!_checkedVertices.Contains(vertexIndex))
             {
-                for (int y = 0; y < nodeCountY; y++)
+                int newOutlineVertex = GetConnectedOutlineVertex(vertexIndex);
+                if (newOutlineVertex != -1)
                 {
-                    var pos = new Vector2(-mapWidth / 2 + x * squareSize + squareSize / 2, -mapHeight / 2 + y * squareSize + squareSize / 2);
-                    controlNodes[x, y] = new ControlNode(pos, caveMap[x, y], squareSize);
+                    _checkedVertices.Add(vertexIndex);
+
+                    var newOutline = new List<int>
+                    {
+                        vertexIndex
+                    };
+                    _outlines.Add(newOutline);
+                    FollowOutline(newOutlineVertex, _outlines.Count - 1);
+                    _outlines[_outlines.Count - 1].Add(vertexIndex);
                 }
             }
+        }
+    }
 
-            squares = new Square[nodeCountX - 1, nodeCountY - 1];
-            for (int x = 0; x < nodeCountX - 1; x++)
+    void FollowOutline(int vertexIndex, int outlineIndex)
+    {
+        _outlines[outlineIndex].Add(vertexIndex);
+        _checkedVertices.Add(vertexIndex);
+        int nextVertexIndex = GetConnectedOutlineVertex(vertexIndex);
+
+        if (nextVertexIndex != -1)
+        {
+            FollowOutline(nextVertexIndex, outlineIndex);
+        }
+    }
+
+    int GetConnectedOutlineVertex(int vertexIndex)
+    {
+        List<Triangle> trianglesContainingVertex = _triangleDictionary[vertexIndex];
+
+        for (int i = 0; i < trianglesContainingVertex.Count; i++)
+        {
+            Triangle triangle = trianglesContainingVertex[i];
+
+            for (int j = 0; j < 3; j++)
             {
-                for (int y = 0; y < nodeCountY - 1; y++)
+                int vertexB = triangle[j];
+                if (vertexB != vertexIndex && !_checkedVertices.Contains(vertexB))
                 {
-                    squares[x, y] = new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x + 1, y], controlNodes[x, y]);
+                    if (IsOutlineEdge(vertexIndex, vertexB))
+                    {
+                        return vertexB;
+                    }
                 }
             }
         }
+
+        return -1;
     }
 
-    public class Square
+    bool IsOutlineEdge(int vertexA, int vertexB)
     {
+        List<Triangle> trianglesContainingVertexA = _triangleDictionary[vertexA];
+        int sharedTriangleCount = 0;
 
-        public ControlNode topLeft, topRight, bottomRight, bottomLeft;
-        public Node centreTop, centreRight, centreBottom, centreLeft;
-        public int configuration;
-
-        public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft)
+        for (int i = 0; i < trianglesContainingVertexA.Count; i++)
         {
-            topLeft = _topLeft;
-            topRight = _topRight;
-            bottomRight = _bottomRight;
-            bottomLeft = _bottomLeft;
+            if (trianglesContainingVertexA[i].Contains(vertexB))
+            {
+                sharedTriangleCount++;
+                if (sharedTriangleCount > 1)
+                {
+                    break;
+                }
+            }
+        }
+        return sharedTriangleCount == 1;
+    }
+}
 
-            centreTop = topLeft.right;
-            centreRight = bottomRight.above;
-            centreBottom = bottomLeft.right;
-            centreLeft = bottomLeft.above;
+public class SquareGrid
+{
+    public Square[,] Squares { get; private set; }
 
-            if (topLeft.caveTile == CaveTile.Wall)
-                configuration += 8;
-            if (topRight.caveTile == CaveTile.Wall)
-                configuration += 4;
-            if (bottomRight.caveTile == CaveTile.Wall)
-                configuration += 2;
-            if (bottomLeft.caveTile == CaveTile.Wall)
-                configuration += 1;
+    public int Width { get => Squares.GetLength(0); }
+    public int Height { get => Squares.GetLength(1); }
+
+    public SquareGrid(CaveTile[,] caveMap, float squareSize)
+    {        
+        int nodeCountX = caveMap.GetLength(0);
+        int nodeCountY = caveMap.GetLength(1);
+        float mapWidth = nodeCountX * squareSize;
+        float mapHeight = nodeCountY * squareSize;
+
+        ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+
+        for (int x = 0; x < nodeCountX; x++)
+        {
+            for (int y = 0; y < nodeCountY; y++)
+            {
+                var pos = new Vector2(-mapWidth / 2 + x * squareSize + squareSize / 2, -mapHeight / 2 + y * squareSize + squareSize / 2);
+                controlNodes[x, y] = new ControlNode(pos, caveMap[x, y], squareSize);
+            }
+        }
+
+        Squares = new Square[nodeCountX - 1, nodeCountY - 1];
+        for (int x = 0; x < nodeCountX - 1; x++)
+        {
+            for (int y = 0; y < nodeCountY - 1; y++)
+            {
+                Squares[x, y] = new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x + 1, y], controlNodes[x, y]);
+            }
+        }
+    }
+}
+
+public class Square
+{
+
+    public ControlNode topLeft, topRight, bottomRight, bottomLeft;
+    public Node centreTop, centreRight, centreBottom, centreLeft;
+    public int configuration;
+
+    public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomRight, ControlNode _bottomLeft)
+    {
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomRight = _bottomRight;
+        bottomLeft = _bottomLeft;
+
+        centreTop = topLeft.right;
+        centreRight = bottomRight.above;
+        centreBottom = bottomLeft.right;
+        centreLeft = bottomLeft.above;
+
+        if (topLeft.caveTile == CaveTile.Wall)
+            configuration += 8;
+        if (topRight.caveTile == CaveTile.Wall)
+            configuration += 4;
+        if (bottomRight.caveTile == CaveTile.Wall)
+            configuration += 2;
+        if (bottomLeft.caveTile == CaveTile.Wall)
+            configuration += 1;
+    }
+}
+public class Node
+{
+    public Vector2 position;
+    public int vertexIndex = -1;
+
+    public Node(Vector2 _pos)
+    {
+        position = _pos;
+    }
+}
+
+public class ControlNode : Node
+{
+    public CaveTile caveTile;
+    public Node above, right;
+
+    public ControlNode(Vector2 _pos, CaveTile _caveTile, float squareSize) : base(_pos)
+    {
+        caveTile = _caveTile;
+        above = new Node(position + Vector2.up * squareSize / 2f);
+        right = new Node(position + Vector2.right * squareSize / 2f);
+    }
+}
+
+public struct Triangle
+{
+    public int vertexIndexA;
+    public int vertexIndexB;
+    public int vertexIndexC;
+    readonly int[] vertices;
+
+    public Triangle(int a, int b, int c)
+    {
+        vertexIndexA = a;
+        vertexIndexB = b;
+        vertexIndexC = c;
+
+        vertices = new int[3];
+        vertices[0] = a;
+        vertices[1] = b;
+        vertices[2] = c;
+    }
+
+    public int this[int i]
+    {
+        get
+        {
+            return vertices[i];
         }
     }
 
-    public class Node
+    public bool Contains(int vertexIndex)
     {
-        public Vector2 position;
-        public int vertexIndex = -1;
-
-        public Node(Vector2 _pos)
-        {
-            position = _pos;
-        }
-    }
-
-    public class ControlNode : Node
-    {
-        public CaveTile caveTile;
-        public Node above, right;
-
-        public ControlNode(Vector2 _pos, CaveTile _caveTile, float squareSize) : base(_pos)
-        {
-            caveTile = _caveTile;
-            above = new Node(position + Vector2.up * squareSize / 2f);
-            right = new Node(position + Vector2.right * squareSize / 2f);
-        }
+        return vertexIndex == vertexIndexA || vertexIndex == vertexIndexB || vertexIndex == vertexIndexC;
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public uint PlayerNumber;
+    public uint ThrottleForce;
+    public uint SteeringSpeed;
+    public Camera CameraPrefab;
+
+    private Rigidbody2D _body;
+    private Camera _camera;
+    private Vector3 _cameraOffset;
+
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _camera = Instantiate(CameraPrefab);
+        _camera.transform.position = new Vector3(transform.position.x, transform.position.y ,0);
+        
+        _cameraOffset = transform.position - _camera.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        float throttle = Mathf.Max(Input.GetAxis($"Player {PlayerNumber} Throttle"), 0);
+        float steering = Input.GetAxis($"Player {PlayerNumber} Steering");
+
+        _body.AddForce(throttle * ThrottleForce * _body.transform.up);
+
+        _body.transform.Rotate(steering * SteeringSpeed * Vector3.forward);
+    }
+
+    private void LateUpdate()
+    {
+        _camera.transform.position = transform.position - _cameraOffset;
+    }
+
+    void SetPlayerNumber(uint number)
+    {
+        PlayerNumber = number;  
+    }
+
+    void SetCamera(Camera camera)
+    {
+        _camera = camera;
+    }
+
+}
