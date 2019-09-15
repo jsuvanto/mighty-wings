e0362dd5b7bd6f6bbad65f605616d342fca95123
Juha Suvanto <jsuvanto@iki.fi>
2019-09-15 10:00:49 +0300

    added weapon base class and initial first weapon

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+    public string Name;
+    public WeaponType Type;
+    public int Damage;
+    public GameObject Ammunition;
+    public int ReloadTime;
+    public int MagazineSize;
+    public int FireRate;
+
+
+    public Weapon()
+    {
+
+    }
+
+    public virtual void Fire()
+    {
+
+    }
+
+    public virtual void Reload()
+    {
+
+    }
+
+}
+
+public enum WeaponType
+{
+    Projectile, Beam, Missile
+}
