8b8592c10dcb9557fd7b58e2c6451c1f7d32ddc2
Juha Suvanto <jsuvanto@iki.fi>
2019-11-30 08:25:28 +0200

    ship explosions now destroyed after effect has played. fixes #36

---
+++ Assets/Scripts/EffectFinished.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectFinished : MonoBehaviour
+{
+    public void OnParticleSystemStopped()
+    {
+        Destroy(transform.parent.gameObject);
+    }
+}
